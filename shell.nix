21a17602f5882bf5abc918bfcc0dcc1291db4f6e
Tiago Castro <tiagolobocastro@gmail.com>
2022-03-04 18:42:00 +0000

    chore(nix): update rust toolchain date

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ profile ? "nightly", date ? "2021-11-22", oxalica ? "194016e6b086bfa5965aeb8979c58b93e03e2485" }:
+{ profile ? "nightly", date ? "2022-02-22", oxalica ? "194016e6b086bfa5965aeb8979c58b93e03e2485" }:
 let
   oxalica_overlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/${oxalica}.tar.gz";
   pkgs = import <nixpkgs> {
