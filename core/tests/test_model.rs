46a4ad8b6bfc2e71a108db30208e8d72d7db6c50
Mattias Eriksson <snaggen@mayam.com>
2022-05-16 18:11:57 +0200

    fmt

--- core/tests/test_model.rs
+++ core/tests/test_model.rs
@@ -1,10 +1,10 @@
 #[test]
-#[cfg(feature = "v2")]  
+#[cfg(feature = "v2")]
 fn test_template_name_with_regex() {
     use paperclip_core::v2::models::{DefaultParameterRaw, Either, ParameterIn};
 
-    let mut op: paperclip_core::v2::models::DefaultOperationRaw =  Default::default();
-    op.parameters = vec![Either::Right(DefaultParameterRaw{
+    let mut op: paperclip_core::v2::models::DefaultOperationRaw = Default::default();
+    op.parameters = vec![Either::Right(DefaultParameterRaw {
         in_: ParameterIn::Path,
         ..Default::default()
     })];
