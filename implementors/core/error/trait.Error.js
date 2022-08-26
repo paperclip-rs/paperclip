eb74a67f1e662cb2cb496a98d25c1e32435f274e
tiagolobocastro <tiagolobocastro@users.noreply.github.com>
2022-08-26 23:39:11 +0000

    deploy: 8b725001bd517ddf53c1e6a5bc449981f837e3b6

---
+++ implementors/core/error/trait.Error.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"paperclip":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"paperclip/enum.PaperClipError.html\" title=\"enum paperclip::PaperClipError\">PaperClipError</a>"]],
+"paperclip_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"paperclip_core/enum.ValidationError.html\" title=\"enum paperclip_core::ValidationError\">ValidationError</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- implementors/std/error/trait.Error.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {
-"paperclip":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"paperclip/enum.PaperClipError.html\" title=\"enum paperclip::PaperClipError\">PaperClipError</a>"]],
-"paperclip_core":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"paperclip_core/enum.ValidationError.html\" title=\"enum paperclip_core::ValidationError\">ValidationError</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
