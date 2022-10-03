392e6018856cc879be4c717de5e01ccc16628e05
Tiago Castro <tiagolobocastro@gmail.com>
2022-10-03 19:19:34 +0100

    chore: add rustup support to nix-shell
    Enable it by default and write out rust-toolchain.toml when entering.
    
    Signed-off-by: Tiago Castro <tiagolobocastro@gmail.com>

--- shell.nix
+++ shell.nix
@@ -1,14 +1,21 @@
-{ profile ? "nightly", date ? "2022-05-06", oxalica ? "43f4c4319fd29d07912a65d405ff03069c7748c4" }:
+{ profile ? "nightly", date ? "2022-05-06", oxalica ? "43f4c4319fd29d07912a65d405ff03069c7748c4", rustup ? true }:
 let
   oxalica_overlay = builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/${oxalica}.tar.gz";
   pkgs = import <nixpkgs> {
     overlays = [ (import oxalica_overlay) ];
   };
+  rust = (pkgs.rust-bin.${profile}.${date}.default.override { extensions = [ "rust-src" ]; });
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    rust-bin.${profile}.${date}.default
     openssl
     pkg-config
-  ];
+  ] ++ pkgs.lib.optional (!rustup) rust;
+  shellHook = ''
+    cat <<EOF >rust-toolchain.toml
+  [toolchain]
+  channel = "${profile}-${date}"
+  components = [ "rust-src" ]
+  EOF
+  '';
 }
