25f17978fa331cf8bfa7899b0e98ea3aff1f4172
tiagolobocastro <tiagolobocastro@users.noreply.github.com>
2024-06-27 20:55:52 +0000

    deploy: 7a31e10b6b47f715073d2084dd379a579d454fd6

---
+++ src-files.js
@@ -0,0 +1,7 @@
+var srcIndex = new Map(JSON.parse('[\
+["paperclip",["",[["v2",[["codegen",[],["author.rs","emitter.rs","impls.rs","mod.rs","object.rs","state.rs"]]],["mod.rs"]]],["error.rs","lib.rs"]]],\
+["paperclip_actix",["",[],["app.rs","lib.rs","web.rs"]]],\
+["paperclip_core",["",[["v2",[],["actix.rs","extensions.rs","mod.rs","models.rs","resolver.rs","schema.rs"]],["v3",[],["contact.rs","external_documentation.rs","header.rs","info.rs","license.rs","mod.rs","openapi.rs","operation.rs","parameter.rs","paths.rs","reference.rs","request_body.rs","response.rs","schema.rs","security_scheme.rs","tag.rs"]]],["error.rs","im.rs","lib.rs","util.rs"]]],\
+["paperclip_macros",["",[],["actix.rs","core.rs","lib.rs"]]]\
+]'));
+createSrcSidebar();
